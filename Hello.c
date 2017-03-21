08223cf549dc381bc3a4c1775184770a3afea028
MendezMartin <martin.mendez.zanet@gmail.com>
2017-03-21 08:43:09 -0300

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,13 @@
+/*
+	Martin Mendez
+	Sintaxis y semantica de los lenguajes - 2017
+	k2051
+	TP #0 - "Hello world!" en C. 
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello world!\n");
+    return 0;
+}
